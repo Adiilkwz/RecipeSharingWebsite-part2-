db6118b41be39805461b4fa9668d45fb917e2980
yerassylkm <240975@astanait.edu.kz>
2026-02-01 04:00:31 +0500

    feat: implement user profile retrieval and update logic with auth middleware

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,34 @@
+const User = require('../models/user');
+
 const getMe = async (req, res) => {
     res.status(200).json(req.user);
 };
 
+const updateUserProfile = async (req, res) => {
+    try {
+        const user = await User.findById(req.user._id);
+
+        if (user) {
+            user.username = req.body.username || user.username;
+            user.email = req.body.email || user.email;
+
+            const updateUser = await user.save();
+
+            res.json({
+                _id: updateUser._id,
+                username: updateUser.username,
+                email: updateUser.email,
+                role: updateUser.role,
+            });
+        } else {
+            res.status(400).json({ message: 'User not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
 module.exports = {
     getMe,
+    updateUserProfile,
 };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { getMe } = require('../controllers/userController');
+const { getMe, updateUserProfile } = require('../controllers/userController');
 
 const{ protect } = require('../middleware/authMiddleware');
 
 router.get('/me', protect, getMe);
+
+router.put('/profile', protect, updateUserProfile);
 
 module.exports = router;
